e9d77db0bcddcfb1fbbf364b4ea87fb59ea30126
ngroegli <nicola@groeg.li>
2025-12-19 18:18:12 +0100

    Bump version to 1.3.0

--- askai.sh
+++ askai.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ask AI Version
-VERSION="1.2.1"
+VERSION="1.3.0"
 
 # Resolve the directory of the script, even if called via symlink
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
