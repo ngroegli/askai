23ae0f52858c3eca5fe385b49354673391b1924d
ngroegli <nicola@groeg.li>
2026-07-27 00:09:20 +0200

    release: 1.3.1

--- src/askai/_version.py
+++ src/askai/_version.py
@@ -6,7 +6,7 @@ All other files should import from here.
 """
 import re
 
-__version__ = "1.3.1-dev"
+__version__ = "1.3.1"
 
 # Parse version info, handling suffixes like -dev, -alpha, etc.
 def _parse_version(version_str):
